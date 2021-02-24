5fe9477bd9c5c8a9a43625da6bb27460d0c799e5
abc <joel.rajesh13@gmail.com>
2021-02-24 11:14:07 +0530

    Flip Box

---
+++ block-BJaaep/code/script.js
@@ -0,0 +1,44 @@
+let wrapper2 =  document.querySelector('.second ');
+let secondBoxes = document.querySelectorAll('.second li'); 
+let boxes = document.querySelectorAll('.first li');
+
+secondBoxes.forEach((element,index) => element.setAttribute("data-text" , index+1))
+
+
+
+boxes.forEach((box, index) => {
+    box.addEventListener('click' , (event) =>{
+        event.target.innerText = index + 1;
+        setTimeout(()=>{
+            event.target.innerText='';
+        },5000)
+    })
+})
+
+
+function getNumber(event){
+    let div = event.target; 
+    if(div.classList == "box"){
+        let number = div.dataset.text;
+        div.innerText =number; 
+       setTimeout(()=>{div.innerText =""},5000)
+    }
+}
+wrapper2.addEventListener('click' , (event)=> {return  getNumber(event)} )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
